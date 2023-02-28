5ff2589562bdc2cb44f13041c1c55750cb7b6618
JelyFishhhhhh_ _ <dlanfongtwb@gmail.com>
2023-02-28 15:34:45 +0800

    Commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from modules import Json
+
+from fastapi import FastAPI
+from uvicorn import Config, Server
+
+from aiofiles import open as aopen
+
+app = FastAPI()
+
+CONFIG = Json.load("config.json")
+
+@app.get("/")
+async def home():
+    return "Hello, World."
+
+def Run():
+    config = Config(app, host = CONFIG["HOST"], port = CONFIG["PORT"])
+    server = Server(config = config)
+    server.run()
+
+if __name__ == "__main__":
+    Run()
